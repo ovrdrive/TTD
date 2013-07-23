5cdd8cf7b6c7d0430dceeab90e48b2ccad8ec09b
CGarcia <cerquit@Cerquits-MacBook-Pro-CGarcia.local>
2013-07-23 16:32:09 +0800

    Initial Commit

---
+++ TicTacDuel/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TicTacDuelAppController.h
+//  TicTacDuel
+//
+//  Created by CGarcia on 7/23/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TicTacDuel/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TicTacDuelAppController.h
+//  TicTacDuel
+//
+//  Created by CGarcia on 7/23/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
